82f54299768bbcaf852194501ac4de6d3806696b
CraigA1987 <48647012+CraigA1987@users.noreply.github.com>
2021-07-18 18:44:20 +0400

    Revert "card added"
    This reverts commit cac9cd6179ad5dc35e31dbcb6e4a143c45baf7fd.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {BrowserModule} from '@angular/platform-browser'
 import {InfoComponent} from './info/info.component';
 import {MapComponent} from './map/map.component';
 import {SearchComponent} from './search/search.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Angular Material
 import {MatTabsModule} from '@angular/material/tabs';
@@ -12,12 +11,6 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatSelectModule} from '@angular/material/select';
-
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -37,13 +30,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTableModule,
     MatPaginatorModule,
     MatProgressBarModule,
-    MatCardModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MatSelectModule
   ],
   providers: [
   ],
