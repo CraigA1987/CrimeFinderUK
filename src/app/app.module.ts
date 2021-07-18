cac9cd6179ad5dc35e31dbcb6e4a143c45baf7fd
CraigA1987 <48647012+CraigA1987@users.noreply.github.com>
2021-07-18 18:33:28 +0400

    card added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {BrowserModule} from '@angular/platform-browser'
 import {InfoComponent} from './info/info.component';
 import {MapComponent} from './map/map.component';
 import {SearchComponent} from './search/search.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Angular Material
 import {MatTabsModule} from '@angular/material/tabs';
@@ -11,6 +12,12 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -30,6 +37,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatTableModule,
     MatPaginatorModule,
     MatProgressBarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
   providers: [
   ],
